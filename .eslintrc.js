578dac396a436fc4c5d59eb78a7d4e0e2e535d7a
yuping.pang <pangyuping@linshimuye.com>
2022-12-08 19:09:31 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "6",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+      // 'linebreak-style': ['error', 'unix'],
+      // 'react-hooks/rules-of-hooks': 'error',
+      // 'react-hooks/exhaustive-deps': 'warn',
+      // 'react/jsx-uses-react': 'error',
+      // 'react/jsx-uses-vars': 'error',
+      // 'react/react-in-jsx-scope': 'error',
+      // 'valid-typeof': [
+      //   'error',
+      //   {
+      //     requireStringLiterals: false
+      //   }
+      // ]
+    }
+}
